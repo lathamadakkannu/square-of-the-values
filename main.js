5d9cd7839864935f52a2e0f6514c45629477c8dd
lathamadakkannu <lathamadakkannu@gmail.com>
2023-10-20 20:43:29 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+/* Write a program in JS to find the square of any number using the function 
+input: 20
+output: 400 */
+
+
+let a=prompt("enter the value")
+let square=(num)=>{
+    return num*num
+}
+console.log(`The square of ${a} is ${square(a)}`);
